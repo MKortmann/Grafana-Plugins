3eb6baf8d1339594a87fbd81bf38418ec7056274
Marcelo Kortmann <marcelo.kortmman@sick.de>
2022-02-07 15:03:35 +0100

    add: simple and responsive barchart with d3

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -3,6 +3,7 @@ import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 // import { css, cx } from 'emotion';
 import { useTheme } from '@grafana/ui';
+import * as d3 from 'd3';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
@@ -11,15 +12,30 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
 
   const values = [4,8,15, 16, 23, 42];
 
-  const barHeight = height / values.length;
+  const scale = d3
+    .scaleLinear()
+    .domain([0, d3.max(values) || 0.0])
+    .range([0, width])
+
+  const axis = d3.axisBottom(scale)
+
+  const padding = 20;
+  const chartHeight = height - padding;
+  const barHeight = chartHeight / values.length;
 
   return (
     <svg width={width} height={height}>
       <g>
         {values.map((value, i) => (
-          <rect x={0} y={i * barHeight} width={value} height={barHeight - 1} fill={theme.palette.greenBase} />
+          <rect x={0} y={i * barHeight} width={scale(value)} height={barHeight - 1} fill={theme.palette.greenBase} />
         ))}
       </g>
+      <g 
+        transform={`translate(0, ${chartHeight})`}
+        ref={node => {
+          d3.select(node).call(axis as any)
+        }}
+      />
     </svg>
   )
 };
